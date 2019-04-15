492ccc665f69c38eae4488a982f3f7efea8cd05f
Karl <karl.chvojka@gmail.com>
2019-04-15 17:00:04 -0400

    Finished Post changes

--- express_server.js
+++ express_server.js
@@ -25,6 +25,7 @@ app.use(cookieSession({
 }))
 
 // GET declaration for the homepage.
+// If user is logged in, redirect to urls, if not, redirect to login. This is done via checking for a session ID
 app.get('/', (req, res) => {
   if (req.session['user_id']) {
     res.redirect('/urls')
@@ -75,24 +76,33 @@ app.get('/urls/new', (req, res) => {
 
 // GET declaration for each individual short URL page.
 app.get('/urls/:shortURL', (req, res) => {
+  // If user is not signed in, send message to login
   if (!req.session['user_id'] || !usersDatabase[req.session['user_id']]) {
     res.send('Please sign in <a href="/login">here</a>')
     return
   }
+  // If the shortURL cannot be found in the database flag an error.
   if (!urlDatabase[req.params.shortURL]) {
     res.send('That url has not been shortened. Please add it and try again.')
     return
   }
+  // if the current logged in user doesnt match the user ID tied to the url your looking at.
   if (usersDatabase[req.session['user_id']].id !== urlDatabase[req.params.shortURL].userID) {
     res.send('Not you url! <a href="/urls">Choose another</a>')
     return
   }
+  // Set variables to be passed through to the view.
   let shortURLRef = req.params.shortURL
   let templateShowVars = { shortURL: shortURLRef, longURL: urlDatabase[shortURLRef].longURL, user: usersDatabase[req.session['user_id']].id }
   res.render('urls_show', templateShowVars)
 })
 
 // POST Declaration for the endpoint /urls
+// Handles submission of new URLs to be shortened.
+// 1. Generates unique ID for the entry.
+// 2. Confirms the submission of the url from the form.
+// 3. Attaches the current viewer ID to the submission.
+// Redirects to the /urls page after successful submission.
 app.post('/urls', (req, res) => {
   var getShortURL = generateRandomString()
   urlDatabase[getShortURL] = {}
@@ -102,15 +112,21 @@ app.post('/urls', (req, res) => {
 })
 
 // POST Declaration for the endpoint /login
+// Handles process of a user logging in.
 app.post('/login', (req, res) => {
+  // Set variables to be used.
   let emailEntry = req.body.email
   let passwordEntry = req.body.password
   let keyEntry = lookupByEmail(emailEntry)
 
-  if (req.body.email === '' || req.body.password === '') {
+  // IF the email, or password entry field are falsy
+  if (!req.body.email || !req.body.password) {
     res.send('You have not entered a username or password. Please try again.')
   }
 
+  // Check if user is registered.
+  // Else if the encrypted password field entry matches that of the user looked up.
+  // Else pass error.
   if (keyEntry === undefined) {
     res.send('Your user does not exist. Please try again.')
   } else if (keyEntry && bcrypt.compareSync(passwordEntry, usersDatabase[keyEntry]['password'])) {
@@ -122,35 +138,42 @@ app.post('/login', (req, res) => {
 })
 
 // POST Declaration for the /logout endpoint.
+// Deletes session cookie and redirects.
 app.post('/logout', (req, res) => {
   req.session = null
   res.redirect('/login')
 })
 
 // POST Declaration for the /register endpoint.
+// Handles registration of a new user.
 app.post('/register', (req, res) => {
+  // Set variables to be used in the process.
   let regVars = usersDatabase
   const usernameEntry = req.body['email']
   const passwordEntry = req.body['password']
   const hashedPassword = bcrypt.hashSync(passwordEntry, 10)
   var randomiD = generateRandomString()
 
-  if (usernameEntry === '' || passwordEntry === '') {
+  // Check if either username or password is empty.
+  if (!usernameEntry || !passwordEntry) {
     res.send('You have not entered a username or password. Please try again.')
     return
   }
+  // Check if username submitted exists in users db.
   for (let user in usersDatabase) {
     if (usersDatabase[user]['email'] === usernameEntry) {
       res.send('That user already exists. Please try again.')
     }
   }
+
+  // Set variables to be used
   regVars[randomiD] = {}
   regVars[randomiD]['id'] = randomiD
   regVars[randomiD]['email'] = usernameEntry
   regVars[randomiD]['password'] = hashedPassword
-
   let emailEntry = req.body.email
   let keyEntry = lookupByEmail(emailEntry)
+
   if (keyEntry === undefined) {
     res.send('Your user does not exist. Please try again.')
   } else if (keyEntry && bcrypt.compareSync(passwordEntry, usersDatabase[keyEntry]['password'])) {
@@ -175,6 +198,8 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 })
 
 // GET Declaration to handle incoming requests from the outside.
+// Looks for the record of the short url being requested.
+// Forwards viewer to applicalble longURL.
 app.get('/u/:shortURL', (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
     const longURL = urlDatabase[req.params.shortURL]['longURL']
@@ -202,7 +227,7 @@ function generateRandomString () {
   return finalStr
 }
 
-// Helper function to identify current users.
+// Helper function to identify current users by crossreferencing current user with ID passed in via form.
 function lookupByEmail (inputUserEntry) {
   var responseID = ''
   Object.keys(usersDatabase).forEach(function (key) {
